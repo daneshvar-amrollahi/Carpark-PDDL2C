3950532f10a9b38772b6197c38036e392b3b339d
Daneshvar Amrollahi <amrollahi.daneshvar@gmail.com>
2020-08-21 22:39:54 +0430

    Extracted Object Lines

--- main.py
+++ main.py
@@ -9,10 +9,33 @@ def readLines():
     result = [line.strip() for line in lines]
     return result
 
+def extractObjects(lines):
+    result = []
+    flag = 0
+    for line in lines:
+        if (line == "(:objects"):
+            flag = 1
+            continue
+        if (line == ")"):
+            flag = 0
+
+        if (flag):
+            result.append(line)
+    return result
+
 def extract():
     lines = readLines()
+    print("All lines: ")
     for line in lines:
         print(line)
+
+    print("--------------")
+
+    objects = extractObjects(lines)
+    print("Object lines: ")
+    for line in objects:
+        print(line)
+    
     #with open(outFile,'w') as o:
     #    for line in lines:
     #        o.write("Current line is:" + line)
