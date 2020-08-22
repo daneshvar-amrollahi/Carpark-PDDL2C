0548dedc471d5120472ebf61b30da8ad06d4faf2
Daneshvar Amrollahi <amrollahi.daneshvar@gmail.com>
2020-08-22 23:28:11 +0430

    new benchmark added

--- ans.c
+++ ans.c
@@ -2,8 +2,8 @@
 #include <klee/klee.h>
 
 #define numOfActions 4
-#define numOfcars 12
-#define numOfcurbs 7
+#define numOfcars 14
+#define numOfcurbs 8
 
 #define car_00 0
 #define car_01 1
@@ -17,6 +17,8 @@
 #define car_09 9
 #define car_10 10
 #define car_11 11
+#define car_12 12
+#define car_13 13
 
 #define curb_0 0
 #define curb_1 1
@@ -25,6 +27,7 @@
 #define curb_4 4
 #define curb_5 5
 #define curb_6 6
+#define curb_7 7
 
 bool at-curb[numOfcars + 1];
 bool at-curb-num[numOfcars + 1][numOfcurbs + 1];
@@ -34,49 +37,54 @@ bool curb-clear[numOfcurbs + 1];
 
 void init()
 {
-	at-curb[car_01] = 1;
-	at-curb-num[car_01][curb_0] = 1;
-	behind-car[car_05][car_01] = 1;
-	car-clear[car_05] = 1;
-	at-curb[car_08] = 1;
-	at-curb-num[car_08][curb_1] = 1;
-	behind-car[car_04][car_08] = 1;
-	car-clear[car_04] = 1;
-	at-curb[car_00] = 1;
-	at-curb-num[car_00][curb_2] = 1;
-	behind-car[car_07][car_00] = 1;
-	car-clear[car_07] = 1;
-	at-curb[car_03] = 1;
-	at-curb-num[car_03][curb_3] = 1;
-	behind-car[car_10][car_03] = 1;
-	car-clear[car_10] = 1;
-	at-curb[car_09] = 1;
-	at-curb-num[car_09][curb_4] = 1;
-	behind-car[car_02][car_09] = 1;
-	car-clear[car_02] = 1;
-	at-curb[car_11] = 1;
-	at-curb-num[car_11][curb_5] = 1;
-	car-clear[car_11] = 1;
 	at-curb[car_06] = 1;
-	at-curb-num[car_06][curb_6] = 1;
-	car-clear[car_06] = 1;
+	at-curb-num[car_06][curb_0] = 1;
+	behind-car[car_04][car_06] = 1;
+	car-clear[car_04] = 1;
+	at-curb[car_02] = 1;
+	at-curb-num[car_02][curb_1] = 1;
+	behind-car[car_08][car_02] = 1;
+	car-clear[car_08] = 1;
+	at-curb[car_07] = 1;
+	at-curb-num[car_07][curb_2] = 1;
+	behind-car[car_10][car_07] = 1;
+	car-clear[car_10] = 1;
+	at-curb[car_11] = 1;
+	at-curb-num[car_11][curb_3] = 1;
+	behind-car[car_03][car_11] = 1;
+	car-clear[car_03] = 1;
+	at-curb[car_01] = 1;
+	at-curb-num[car_01][curb_4] = 1;
+	behind-car[car_09][car_01] = 1;
+	car-clear[car_09] = 1;
+	at-curb[car_12] = 1;
+	at-curb-num[car_12][curb_5] = 1;
+	behind-car[car_13][car_12] = 1;
+	car-clear[car_13] = 1;
+	at-curb[car_05] = 1;
+	at-curb-num[car_05][curb_6] = 1;
+	behind-car[car_00][car_05] = 1;
+	car-clear[car_00] = 1;
+	curb-clear[curb_7] = 1;
 }
 
 void goal()
 {
 	return(
 		at-curb-num[car_00][curb_0] == 1 && 
-		behind-car[car_07][car_00] == 1 && 
+		behind-car[car_08][car_00] == 1 && 
 		at-curb-num[car_01][curb_1] == 1 && 
-		behind-car[car_08][car_01] == 1 && 
+		behind-car[car_09][car_01] == 1 && 
 		at-curb-num[car_02][curb_2] == 1 && 
-		behind-car[car_09][car_02] == 1 && 
+		behind-car[car_10][car_02] == 1 && 
 		at-curb-num[car_03][curb_3] == 1 && 
-		behind-car[car_10][car_03] == 1 && 
+		behind-car[car_11][car_03] == 1 && 
 		at-curb-num[car_04][curb_4] == 1 && 
-		behind-car[car_11][car_04] == 1 && 
+		behind-car[car_12][car_04] == 1 && 
 		at-curb-num[car_05][curb_5] == 1 && 
-		at-curb-num[car_06][curb_6] == 1 
+		behind-car[car_13][car_05] == 1 && 
+		at-curb-num[car_06][curb_6] == 1 && 
+		at-curb-num[car_07][curb_7] == 1 
 	)
 }void move_curb_to_curb(int car, int curbsrc, int curbdest)
 {
