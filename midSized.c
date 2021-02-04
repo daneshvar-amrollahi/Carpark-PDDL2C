1e9da681a1121aa890fec1e275e956aaa6978303
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-02-04 14:49:13 +0330

    Added mid-sized benchmark with 7 cars 4 curbs

--- midSized.c
+++ midSized.c
@@ -32,34 +32,37 @@ int at_curb_num[numOfcars + 1][numOfcurbs + 1];
 int behind_car[numOfcars + 1][numOfcars + 1];
 int car_clear[numOfcars + 1];
 int curb_clear[numOfcurbs + 1];
+/*
+; =========== INIT =========== 
+;  curb_0: car_00 car_01 
+;  curb_1: car_02 car_03 
+;  curb_2: car_04 car_05
+;  curb_3: car_06
+; ========== /INIT =========== 
 
+; =========== GOAL =========== 
+;  curb_0: car_00 car_05 
+;  curb_1: car_03 car_02 
+;  curb_2: car_03 car_02 
+;  curb_3: car_06
+; =========== /GOAL =========== 
+*/
 void init()
 {
-	at_curb[car_01] = 1;
-	at_curb_num[car_01][curb_0] = 1;
-	behind_car[car_05][car_01] = 1;
-	car_clear[car_05] = 1;
-	at_curb[car_08] = 1;
-	at_curb_num[car_08][curb_1] = 1;
-	behind_car[car_04][car_08] = 1;
-	car_clear[car_04] = 1;
 	at_curb[car_00] = 1;
-	at_curb_num[car_00][curb_2] = 1;
-	behind_car[car_07][car_00] = 1;
-	car_clear[car_07] = 1;
-	at_curb[car_03] = 1;
-	at_curb_num[car_03][curb_3] = 1;
-	behind_car[car_10][car_03] = 1;
-	car_clear[car_10] = 1;
-	at_curb[car_09] = 1;
-	at_curb_num[car_09][curb_4] = 1;
-	behind_car[car_02][car_09] = 1;
-	car_clear[car_02] = 1;
-	at_curb[car_11] = 1;
-	at_curb_num[car_11][curb_5] = 1;
-	car_clear[car_11] = 1;
+	at_curb_num[car_00][curb_0] = 1;
+	behind_car[car_01][car_00] = 1;
+	car_clear[car_01] = 1;
+	at_curb[car_02] = 1;
+	at_curb_num[car_02][curb_1] = 1;
+	behind_car[car_03][car_02] = 1;
+	car_clear[car_03] = 1;
+	at_curb[car_04] = 1;
+	at_curb_num[car_04][curb_2] = 1;
+	behind_car[car_05][car_04] = 1;
+	car_clear[car_05] = 1;
 	at_curb[car_06] = 1;
-	at_curb_num[car_06][curb_6] = 1;
+	at_curb_num[car_06][curb_3] = 1;
 	car_clear[car_06] = 1;
 }
 
@@ -67,19 +70,14 @@ int goal()
 {
 	int correct = 
 		(at_curb_num[car_00][curb_0] == 1) +
-		(behind_car[car_07][car_00] == 1) + 
-		(at_curb_num[car_01][curb_1] == 1) + 
-		(behind_car[car_08][car_01] == 1) + 
-		(at_curb_num[car_02][curb_2] == 1) + 
-		(behind_car[car_09][car_02] == 1) + 
-		(at_curb_num[car_03][curb_3] == 1) + 
-		(behind_car[car_10][car_03] == 1) + 
-		(at_curb_num[car_04][curb_4] == 1) + 
-		(behind_car[car_11][car_04] == 1) + 
-		(at_curb_num[car_05][curb_5] == 1) + 
-		(at_curb_num[car_06][curb_6] == 1); 
+		(behind_car[car_05][car_00] == 1) + 
+		(at_curb_num[car_02][curb_1] == 1) + 
+		(behind_car[car_01][car_02] == 1) + 
+		(at_curb_num[car_03][curb_2] == 1) + 
+		(behind_car[car_02][car_03] == 1) + 
+		(at_curb_num[car_06][curb_3] == 1) +  
 	printf("%d\n", correct);
-	return (correct == 12);
+	return (correct == 7);
 }
 
 void move_curb_to_curb(int car, int curbsrc, int curbdest)
